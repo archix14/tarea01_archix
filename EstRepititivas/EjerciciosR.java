34b985019e4701e28c1e388f15195388f03203c0
Fredy Rivaldo <fredy.maman@upeu.edu.pe>
2022-05-12 17:57:19 +0000

    social

--- EstRepititivas/EjerciciosR.java
+++ EstRepititivas/EjerciciosR.java
@@ -1,5 +1,7 @@
 package EstRepititivas;
 
+import java.util.Scanner;
+
 /**
  * EjerciciosR
  */
@@ -30,15 +32,36 @@ public class EjerciciosR {
 
     public static void imprimirForN1_20() {
         for(int numInit = 1; numInit <=20; numInit++){
-         System.out.println(numInit);   
-        }
-        
+            System.out.println(numInit);   
+           }
     }
 
-
+    public static void nombrevertical() {
+        Scanner lt=new Scanner(System.in);
+        System.out.println("ingrese  su nombre");
+        String nombre=lt.next();
+        for (int i =0;i < nombre.length(); i++) {
+        System.out.println(nombre.charAt(i));
+            
+        }
+    
+       
+       
+       
+    }
+     public static void nombreinverso() {
+         Scanner lt=new Scanner(System.in);
+         System.out.println("ingrese su nombre");
+         String nombre=lt.next();
+         for (int i = nombre.length()-1; i >=0, i--){
+          System.out.println(nombre.charAt(i));
+     }
+  
     public static void main(String[] args) {
-        System.out.println("Imprime while:"); imprimirWhileN1_20();
+        /*System.out.println("Imprime while:"); imprimirWhileN1_20();
         System.out.println("Imprime do while:"); imprimirWhileN1_20();
-        System.out.println("imprime For:"); imprimirForN1_20();
+        System.out.println("imprime For:"); imprimirForN1_20();*/
+
+        nombreinverso();
     }
 }
