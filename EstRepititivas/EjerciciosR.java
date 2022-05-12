fc55f37d42bbe4f90c7cc7e4d76510ce7772a094
Fredy Rivaldo <fredy.maman@upeu.edu.pe>
2022-05-12 16:58:51 +0000

    archix

---
+++ EstRepititivas/EjerciciosR.java
@@ -0,0 +1,44 @@
+package EstRepititivas;
+
+/**
+ * EjerciciosR
+ */
+public class EjerciciosR {
+       
+    
+    public static void imprimirWhileN1_10() {
+        int numInit=21;
+        while (numInit<=20) {
+        System.out.println(numInit);  
+        numInit=numInit+1;
+         
+        }
+            }
+
+
+    public static void imprimirWhileN1_20() {
+        for(int numInit = 1; numInit <=20; numInit++)
+        System.out.println(numInit);
+    
+
+        int numInit=21;
+        do {
+            System.out.println(numInit);
+            numInit++;
+        } while (numInit<=20);
+    }
+
+    public static void imprimirForN1_20() {
+        for(int numInit = 1; numInit <=20; numInit++){
+         System.out.println(numInit);   
+        }
+        
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Imprime while:"); imprimirWhileN1_20();
+        System.out.println("Imprime do while:"); imprimirWhileN1_20();
+        System.out.println("imprime For:"); imprimirForN1_20();
+    }
+}
