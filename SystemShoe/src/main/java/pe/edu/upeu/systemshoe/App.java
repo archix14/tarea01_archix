baa0cbfaba494ea691278bb742b0e5676e260b3f
archix14 <Fredyrivaldo75@gmail.com>
2022-07-19 15:45:19 -0500

    Shoe

---
+++ SystemShoe/src/main/java/pe/edu/upeu/systemshoe/App.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.systemshoe;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
