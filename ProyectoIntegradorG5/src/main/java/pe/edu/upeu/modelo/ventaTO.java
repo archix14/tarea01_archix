467c1791962329c8a431c5f5f8e951b9d303b45f
archix14 <Fredyrivaldo75@gmail.com>
2022-07-20 18:47:29 -0500

    tarea02

--- ProyectoIntegradorG5/src/main/java/pe/edu/upeu/modelo/ventaTO.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.modelo;
-
-import lombok.Data;
-
-@Data
-public class VentaTO {    
-   public String idVenta, dni, fecha;
-   public double descuento,subprecio, igv, precioTotal;
-}
